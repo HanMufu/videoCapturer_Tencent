3d7e1afcaee376929329d9479deb54d8eb5ca461
hanmufu <hanmufu@gmail.com>
2018-10-21 11:55:13 +0800

    logger added, can atuomatically detected computer's user name now

--- start.py
+++ start.py
@@ -7,6 +7,10 @@ Created on Sat Oct 20 15:50:20 2018
 """
 
 import os
+import logging
+logger_name = "myLog"
+logger = logging.getLogger(logger_name)
+logger.setLevel(logging.DEBUG)
 
 # 获取文件夹下除了隐藏文件之外的文件
 def getLocalNormalFiles():
@@ -24,10 +28,13 @@ def rmTempFiles():
         if listFiles[i][0:13] == 'videoCapturer':
             os.system("rm %s" % listFiles[i])
 
-os.system("cd")
-os.chdir("..")
+# e.g. hanmufu
+usr = (os.getcwd().split('/'))[2]
+# e.g. /Users/hanmufu
+usr_path = '/Users/%s' % usr
+
 # 进入腾讯视频下载文件夹
-os.chdir("%s/Library/Containers/com.tencent.tenvideo/Data/Library/Application Support/Download/video" % os.getcwd())
+os.chdir("%s/Library/Containers/com.tencent.tenvideo/Data/Library/Application Support/Download/video" % usr_path)
 # 进入地球脉动第二集的文件夹
 os.chdir("./q0022m7d26n.321004.hls")
 Folders = getLocalNormalFiles()
@@ -38,18 +45,19 @@ for i in range(len(Folders)):
     videos = getLocalNormalFiles()
     videos.sort(key= lambda x:int(x[:-3]))
     cmd_allVideosInOneFolder = ' '.join(videos)
-    print("..floder \"%s\" has been done" % Folders[i])
-    os.system("cat %s > /Users/hanmufu/Downloads/videoCapturer%s.ts" % (cmd_allVideosInOneFolder, str(int(int(videos[0][:-3]) / 30))))
+    logging.info("..floder \"%s\" has been done" % Folders[i])
+    os.system("cat %s > %s/Downloads/videoCapturer%s.ts" % (cmd_allVideosInOneFolder, usr_path, str(int(int(videos[0][:-3]) / 30))))
     os.chdir("..")
 allCapturedVideos = []
 for i in range(len(Folders)):
     allCapturedVideos.append("videoCapturer%s.ts" % i)
 cmd_allCapturedVideos = ' '.join(allCapturedVideos)
-os.chdir("/Users/hanmufu/Downloads")
+os.chdir("%s/Downloads" % usr_path)
 os.system("cat %s > enjoy.ts" % cmd_allCapturedVideos)
-print("...videos has been put together")
+logging.info("...videos has been put together")
 rmTempFiles()
-print("...deleted temp files")
-print("...ALL DONE")
+logging.info("...deleted temp files")
+logging.info("...ALL DONE")
+#print("ALL DONE")
     
 
